8cdd85831691df8cced1445b29ab60ee8352c670
Danish-chouhan <danishchouhan41@gmail.com>
2023-01-11 13:18:12 +0530

    DEEP-JAVASCRIPT-V3

---
+++ Course-Deep-javaScript-v3/js/introduction/introduction.js
@@ -0,0 +1,18 @@
+var a = 10
+a++ //this is increment
+document.write(a + "<br>")
+++a //this is pre increment
+document.write(a + "<br>")
+ //increment means a++ => (a + 1)
+
+// kylesimpson say there is no differense between
+
+// --**--
+
+
+var b = '1'
+b = b + 1 //  when we use this style of increment then tis will not increment it will concat there integers
+document.write(b + "<br>")
+
+b++ // this is we called postfix style of incremenr it will be increment
+document.write(b)
