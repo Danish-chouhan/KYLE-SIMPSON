b7631522ec98d4b944af175b86bf25be37eb5e6e
Danish-chouhan <danishchouhan41@gmail.com>
2023-01-12 02:39:39 +0530

    DEEP-JAVASCRIPT-V3

---
+++ Course-Deep-javaScript-v3/js/INTRODUCTION/Course-Overview.js
@@ -0,0 +1,11 @@
+// course Overview
+
+// types
+// primitive types , abstract operation , coercion , equality , TypeScript,Flow,etc.
+
+
+// scope
+// nested scope , hoisting , closure , Module
+
+// Object(Oriented)
+// this , class{} , prototypes , OO vs. OLOO
