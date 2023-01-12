3492c583e4e1b5dd75ab0d33e9cd7debb6598b83
Danish-chouhan <danishchouhan41@gmail.com>
2023-01-12 23:01:53 +0530

    DEEP-JAVASCRIPT-V3

---
+++ Course-Deep-javaScript-v3/js/TYPES/Primitive-Types.js
@@ -0,0 +1,20 @@
+// PRIMITIVE TYPES 
+
+// there are some difference between primitive type and non primitive types
+
+//these are types 
+// UNDEFINED
+// STRING
+// NUMBER
+// BOOLEAN
+// OBJECT
+// SYMBOL
+
+// OTHER TYPES
+// UNDECLERED
+// NULL
+// FUNCTION
+// ARRAY
+// BRIGINT
+
+// these are types
