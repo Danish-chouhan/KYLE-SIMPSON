4bcf508e3525c6adfa9c75d72511ed1d7d5f8891
Danish-chouhan <danishchouhan41@gmail.com>
2023-01-13 12:32:03 +0530

    DEEP-JAVASCRIPT-V3

--- Course-Deep-javaScript-v3/js/TYPES/kinds-of-empetines.js
+++ Course-Deep-javaScript-v3/js/TYPES/kinds-of-empetines.js
@@ -1,1 +1,9 @@
 // empty becouse there is no syntax cheack out the md
+
+var a;
+console.log(typeof a) // undefined
+
+document.write(b) // this is undecleard becouse there is no variable for b
+
+// var = 1;
+console.log(typeof a) // uninitialized
