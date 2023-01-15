526d7c8fcfc92e94fe437a2b34fb77acda8eea0e
Danish-chouhan <danishchouhan41@gmail.com>
2023-01-15 14:15:11 +0530

    bugfix/added-link/Readme

--- Course-Deep-javaScript-v3/js/TYPES/Nagative-Zero.js
+++ Course-Deep-javaScript-v3/js/TYPES/Nagative-Zero.js
@@ -11,3 +11,31 @@ console.log(danish > 0);
 
 console.log(Object.is(danish,-0)) // this will count like this hw makes two parameter and it will give you 1st parameter name and secound is value if they both are equal then it will show you true
 console.log(Object.is(danish,0));
+
+// ----***----
+
+console.log(Math.sign(-3));
+console.log(Math.sign(3))
+console.log(Math.sign(-0));
+console.log(Math.sign(0));
+
+function chouhan(x){
+    return x !== 0 ? Math.sign(x) : Object.is(x,-0)? -1 : 1;
+}
+
+console.log(chouhan(-3));
+console.log(chouhan(3));
+console.log(chouhan(0));
+console.log(chouhan(-0))
+
+// ---***---
+
+function DanishChouhan(z){
+    var direction = 
+    (z < 0 || Object.is(z,-0)) ? "⇓" : "⇑";
+    return `${direction} ${Math.abs(z)}`
+}
+console.log(DanishChouhan(-3))
+console.log(DanishChouhan(3))
+console.log(DanishChouhan(-0))
+console.log(DanishChouhan(0))
