c581c6ac703df27a9313f612fe250ae25373c9a2
Danish-chouhan <danishchouhan41@gmail.com>
2023-01-16 02:06:05 +0530

    DEEP-JAVASCRIPT-V3

---
+++ Course-Deep-javaScript-v3/js/TYPES/Fundamental-Object.js
@@ -0,0 +1,28 @@
+// fundamenta Object
+// 1 - built-in-object
+// 2 - Netive Function
+
+
+// new usedfull where and where not
+
+// use NEW
+
+// 1 - Object()
+// 2 - array()
+// 3 - Function()
+// 4 - Date()
+// 5 - RegExp()
+// 6 - Errror()
+
+//       ----
+
+// not usefull new
+
+// 1 - String() 
+// 2 - Number()
+// 3 - Boolean()
+
+
+// Example of (New) with date method
+var  a = new Date("august 12 , 2023")
+document.write(a)
